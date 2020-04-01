5b377aeb499aa4b49ef5442436e6c4e51a27baa8
Khadija <62945269+Khadija-Asim@users.noreply.github.com>
2020-04-01 20:58:34 +0500

    using if

---
+++ use of if.py
@@ -0,0 +1,8 @@
+Username_set="khadija"
+Password_set="2007"
+
+Username=input("enter your username")
+Password=input("enter your password")
+
+if("Username_set==username")and("Password_set==password"):
+    print("you logged in succesfully")
