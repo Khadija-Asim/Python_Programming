250ae7fc3c8e46d50ae2ac8cd093c29d04503aeb
Khadija <62945269+Khadija-Asim@users.noreply.github.com>
2020-04-01 20:41:03 +0500

    finding gain points

---
+++ percentage (2).py
@@ -0,0 +1,14 @@
+nido=int(input("real price of nido......."))
+blueband=int(input("real price of blueband.......")) 
+nutella=int(input("real price of nutella......."))
+nuggets=int(input("real price of nuggets........"))
+real_prices=nido+blueband+nutella+nuggets
+print(real_prices)
+NIDO=int(input("price i paid for nido"))
+BLUEBAND=int(input("price i paid for blueband"))
+NUTELLA=int(input("price i paid for nutella"))
+NUGGETS=int(input("price i paid for nuggets"))
+market_price=NIDO+BLUEBAND+NUTELLA+NUGGETS
+print(market_price)
+gain_in_price=(real_prices/market_price)*100
+print(gain_in_price)
