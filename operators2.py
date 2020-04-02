282cad25503eaa1a2c935b3e212d4b29534af574
Khadija <62945269+Khadija-Asim@users.noreply.github.com>
2020-04-02 14:07:37 +0500

    adding operators

---
+++ operators2.py
@@ -0,0 +1,14 @@
+khadija_age =12
+erum_age=26
+equalto=khadija_age==erum_age
+not_equalto=khadija_age!=erum_age
+less_than=khadija_age<erum_age
+greater_than=khadija_age>erum_age
+lessthan_equalto=khadija_age<=erum_age
+greaterthan_equalto=khadija_age>=erum_age
+print(equalto)
+print(not_equalto)
+print(less_than)
+print(greater_than)
+print(lessthan_equalto)
+print(greaterthan_equalto)
