9e83bf4211f7e4feff6c52210a74aa3eb909d4a6
Khadija <62945269+Khadija-Asim@users.noreply.github.com>
2020-04-14 16:00:19 +0500

    mad_lib

---
+++ mad lib game.py
@@ -0,0 +1,12 @@
+print("NOW WE PLAY A MAD LIB GAME")
+
+color=input("enter a color:")
+singular_noun=input("enter a singular_noun:")
+celebrity=input("enter a celebrity:")
+print("...........................................")
+print("Rosses are"  +  color)
+print(singular_noun  + " is blue")
+print("i love"  + celebrity)
+
+
+print("you win!")
